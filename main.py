fd55f9b594a9d737638d76ff3e685e257dfe5f7b
Suman Medda <sumanmedda@Sumans-MacBook-Air.local>
2023-11-06 11:39:57 +0530

    initialized webcam

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,720)
+cap.set(3,640)
+cap.set(4,480)
 
 imgBackground = cv2.imread('Resources/background.png')
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Face Attendance", img)
+    imgBackground[162:162+480,55:55+640] = img
+    cv2.imshow("webcam", img)
     cv2.imshow("Face Attendance", imgBackground)
     cv2.waitKey(1)
