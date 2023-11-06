e4ac681dc28312c73b48996ec90a734931c67758
Suman Medda <sumanmedda@Sumans-MacBook-Air.local>
2023-11-06 12:02:54 +0530

    encoding ids done

--- encode_gen.py
+++ encode_gen.py
@@ -12,3 +12,22 @@ studentIds = []
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath,path)))
     studentIds.append(os.path.splitext(path)[0])
+
+def findEncodings(imagesList):
+    encodeList = [ ]
+    for img in imagesList:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    
+    return encodeList
+
+# Encoding
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown,studentIds]
+
+file = open("EncodeFile.p","wb")
+pickle.dump(encodeListKnownWithIds,file)
+file.close()
+#  saving the list of encoding list with student ids
+
