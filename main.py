00dfcd5e501ebfa2e711e77ce578b568d557e3f3
Suman Medda <sumanmedda@Sumans-MacBook-Air.local>
2023-11-07 17:29:04 +0530

    face attendance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# NOTE : Images sizes are set to : 216 x 216
 # importing the cv2 package for video capture
+from datetime import datetime
 import os
 import pickle
 import cv2
@@ -61,46 +63,82 @@ while True:
     imgBackground[162:162+480,55:55+640] = img
     imgBackground[44:44+633,808:808+414] = imgModeList[modeType]
     
-    for encodeFace, faceLoc in zip(encodeCurrFrame,faceCurrFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
-        matchIndex = np.argmin(faceDis)
+    if faceCurrFrame:
+        for encodeFace, faceLoc in zip(encodeCurrFrame,faceCurrFrame):
+            matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+            matchIndex = np.argmin(faceDis)
+            
+            if matches[matchIndex]:
+                y1,x2,y2,x1 = faceLoc
+                y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+                bbox = 55+x1,162+y1,x2-x1,y2-y1
+                imgBackground = cvzone.cornerRect(imgBackground,bbox,rt=0)
+                id = studentIds[matchIndex]
+                if counter == 0:
+                    cvzone.putTextRect(imgBackground,"Loading...",(275,400))
+                    cv2.imshow("Face Attendance", imgBackground)
+                    cv2.waitKey(1)
+                    counter = 1
+                    modeType = 1
         
-        if matches[matchIndex]:
-            y1,x2,y2,x1 = faceLoc
-            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
-            bbox = 55+x1,162+y1,x2-x1,y2-y1
-            imgBackground = cvzone.cornerRect(imgBackground,bbox,rt=0)
-            id = studentIds[matchIndex]
-            if counter == 0:
-                counter = 1
-                modeType = 1
-    
-    if counter != 0:
-        # if counter is 1 then downloading the data from db
-        if counter == 1:
-            # getting student info
-            studentInfo = db.reference(f'students/{id}').get()
-            # getting images of students from storage and joining with image extension saved in firebase storage
-            blob = bucket.get_blob(f'images/{id}.jpg')
-            array = np.frombuffer(blob.download_as_string(), np.uint8)
-            imgStudent = cv2.imdecode(array,cv2.COLOR_BGRA2BGR)
-        
-        # adjusting and showing the downloaded data on the screen
-        cv2.putText(imgBackground, str(studentInfo['total_attendance']),(861,125),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
-        cv2.putText(imgBackground, str(studentInfo['major']),(1006,550),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
-        cv2.putText(imgBackground, str(studentInfo['id']),(1006,493),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
-        cv2.putText(imgBackground, str(studentInfo['standings']),(910,625),cv2.FONT_HERSHEY_COMPLEX,0.6,(100,100,100),1)
-        cv2.putText(imgBackground, str(studentInfo['year']),(1025,625),cv2.FONT_HERSHEY_COMPLEX,0.6,(100,100,100),1)
-        cv2.putText(imgBackground, str(studentInfo['starting_year']),(1125,625),cv2.FONT_HERSHEY_COMPLEX,0.6,(100,100,100),1)
-        # centering text of name as it could be of any length
-        (w,h), _ = cv2.getTextSize(studentInfo['name'],cv2.FONT_HERSHEY_COMPLEX,1,1)
-        offset = (414-w)//2 
-        cv2.putText(imgBackground, str(studentInfo['name']),(808+offset,445),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,50),1)
-        
-        imgBackground[175:175+216,909:909+216] = imgStudent
-        
-        counter += 1
+        if counter != 0:
+            # if counter is 1 then downloading the data from db
+            if counter == 1:
+                # getting student info
+                studentInfo = db.reference(f'students/{id}').get()
+                # getting images of students from storage and joining with image extension saved in firebase storage
+                blob = bucket.get_blob(f'images/{id}.jpg')
+                array = np.frombuffer(blob.download_as_string(), np.uint8)
+                imgStudent = cv2.imdecode(array,cv2.COLOR_BGRA2BGR)
+                # update Data of attendance time
+                dateTimeObject = datetime.strptime(studentInfo['last_attendance_time'], "%Y-%m-%d %H:%M:%S"),
+                secondsElapsed = (datetime.now()-dateTimeObject).total_seconds()
+                # need to add one day logic that after 1 day only student can mark present
+                if secondsElapsed > 30:    
+                    # update Data of attendance
+                    ref = db.reference(f'students/{id}')
+                    studentInfo['total_attendance'] += 1
+                    ref.child('total_attendance').set(studentInfo['total_attendance'])
+                    ref.child('last_attendance_time').set(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                else:
+                    modeType = 3
+                    counter = 0
+                    imgBackground[44:44+633,808:808+414] = imgModeList[modeType]
+                    
+                    
+            if modeType != 3:
+                if 10<counter<20:
+                    modeType = 2
+                
+                imgBackground[44:44+633,808:808+414] = imgModeList[modeType]
+                    
+                if counter <= 10:
+                    # adjusting and showing the downloaded data on the screen
+                    cv2.putText(imgBackground, str(studentInfo['total_attendance']),(861,125),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
+                    cv2.putText(imgBackground, str(studentInfo['major']),(1006,550),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
+                    cv2.putText(imgBackground, str(studentInfo['id']),(1006,493),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
+                    cv2.putText(imgBackground, str(studentInfo['standings']),(910,625),cv2.FONT_HERSHEY_COMPLEX,0.6,(100,100,100),1)
+                    cv2.putText(imgBackground, str(studentInfo['year']),(1025,625),cv2.FONT_HERSHEY_COMPLEX,0.6,(100,100,100),1)
+                    cv2.putText(imgBackground, str(studentInfo['starting_year']),(1125,625),cv2.FONT_HERSHEY_COMPLEX,0.6,(100,100,100),1)
+                    # centering text of name as it could be of any length
+                    (w,h), _ = cv2.getTextSize(studentInfo['name'],cv2.FONT_HERSHEY_COMPLEX,1,1)
+                    offset = (414-w)//2 
+                    cv2.putText(imgBackground, str(studentInfo['name']),(808+offset,445),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,50),1)
+                    
+                    imgBackground[175:175+216,909:909+216] = imgStudent
+            
+                counter += 1
+                
+                if counter >= 20:
+                    counter = 0
+                    modeType = 0
+                    studentInfo = []
+                    imgStudent = []
+                    imgBackground[44:44+633,808:808+414] = imgModeList[modeType]
+    else:
+        modeType = 0
+        counter = 0        
             
     
     cv2.imshow("webcam", img)
